a5074df78a1017adae2399691b623069a508d0cf
ybelMekk <youssef.bel.mekki@nav.no>
2022-02-18 13:45:24 +0100

    fix: filepath for find command

--- pkg/commands/find.go
+++ pkg/commands/find.go
@@ -26,26 +26,36 @@ var findCmd = &cobra.Command{
 		if artifact == "" {
 			return errors.New("missing artifact")
 		}
-		path := PathFlags.RepoDir
+
+		path, err := PathFlags.WorkDir()
+		if err != nil {
+			return err
+		}
+
 		files, err := ioutil.ReadDir(path)
 		if err != nil {
 			return fmt.Errorf("could not read dir %w", err)
 		}
 
 		for _, file := range files {
-			var attFilePath = "./" + path + "/" + file.Name()
+			var attFilePath = fmt.Sprintf("%s/%s", path, file.Name())
 
 			if ext := filepath.Ext(file.Name()); ext != ".att" {
 				continue
 			}
 
-			fileContents, _ := os.ReadFile(attFilePath)
+			fileContents, err := os.ReadFile(attFilePath)
+			if err != nil {
+				return fmt.Errorf("read .att file content %s, %w", attFilePath, err)
+			}
+
 			provenance, err := intoto.ParseEnvelope(fileContents)
 			if err != nil {
 				return fmt.Errorf("could not read file %s, %w", attFilePath, err)
 			}
 			result := intoto.FindMaterials(provenance.Predicate.Materials, artifact)
 			app := strings.Split(file.Name(), ".")[0]
+
 			if len(result) == 0 {
 				log.Infof("no dependcies where found in app %s", app)
 			} else {
