54e15ef153ac08129960e3b282e1cddd06cad609
ybelMekk <youssef.bel.mekki@nav.no>
2022-02-23 15:03:41 +0100

    fix: some fresh up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh -l
 
-echo "------- Preparing spicy Slsa in container with repository: $SALSA_SCAN_REPO -------"
+echo "---------- Preparing pico-de-galo Slsa container with repository: $SALSA_SCAN_REPO ----------"
 
-ls -l
 # Run salsa commands
 # --repoDir "$SALSA_SCAN_REPO_DIR" \
 salsa scan \
@@ -10,13 +9,13 @@ salsa scan \
   --github_context "$SALSA_SCAN_GITHUB_CONTEXT" \
   --runner_context "$SALSA_SCAN_RUNNER_CONTEXT" \
   --env_context "" &&
-  echo "------- Slsa provenance for repository $SALSA_SCAN_REPO generated -------" &&
+  echo "---------- Slsa roja provenance for repository: $SALSA_SCAN_REPO generated ----------" &&
   salsa attest \
     --repo "$SALSA_SCAN_REPO" \
     --config salsa-sample.yaml "$SALSA_REPO_IMAGE" &&
-  echo "------- Slsa provenance $SALSA_SCAN_REPO signed and uploaded -------" &&
+  echo "---------- Slsa verde provenance for: $SALSA_SCAN_REPO signed and uploaded to remote host ----------" &&
   salsa attest \
     --verify \
     --repo "$SALSA_SCAN_REPO" \
     --config salsa-sample.yaml "$SALSA_REPO_IMAGE" &&
-  echo "------- Attest verified for $SALSA_SCAN_REPO -------"
+  echo "---------- Slsa aguacate attest verified for: $SALSA_SCAN_REPO ----------"
