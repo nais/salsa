44fed02f4ea7e03a2344fb6398b7343e7f6f5276
ybelMekk <youssef.bel.mekki@nav.no>
2022-02-23 15:36:06 +0100

    add: more debug print

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ echo "---------- Preparing pico-de-galo Slsa container with repository: $SALSA_S
 
 # Run salsa commands
 # --repoDir "$SALSA_SCAN_REPO_DIR" \
+ls -ls
+
 salsa scan \
   --repo "$SALSA_SCAN_REPO" \
   --github_context "$SALSA_SCAN_GITHUB_CONTEXT" \
