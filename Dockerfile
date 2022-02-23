69bcc0b6ed7f7edf75dd81c6e589f1baa0a6b37b
ybelMekk <youssef.bel.mekki@nav.no>
2022-02-23 10:35:31 +0100

    test: if path works in github

--- Dockerfile
+++ Dockerfile
@@ -29,14 +29,10 @@ COPY --from=builder /src/bin/salsa /usr/local/bin/
 RUN apk add --no-cache ca-certificates git curl
 RUN curl -L -f https://github.com/sigstore/cosign/releases/download/v1.5.1/cosign-linux-amd64 > /usr/local/bin/cosign && chmod +x /usr/local/bin/cosign
 
-#RUN export PATH=$PATH:/app
-
 RUN chmod +x /usr/local/bin/salsa
 
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
-
-ADD . /
 
 # Set the binary as the entrypoint of the container
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,22 +4,20 @@ echo "------- Running container with repo $SALSA_SCAN_REPO -------"
 
 # Run salsa commands
 salsa scan \
+  --repoDir "$SALSA_SCAN_REPO_DIR" \
   --repo "$SALSA_SCAN_REPO" \
   --github_context "$SALSA_SCAN_GITHUB_CONTEXT" \
   --runner_context "$SALSA_SCAN_RUNNER_CONTEXT" \
   --env_context "" &&
-
-echo "------- Salsa provenance $SALSA_SCAN_REPO generated -------" &&
-
-salsa attest \
-  --repo "$SALSA_SCAN_REPO" \
-  --config salsa-sample.yaml "$SALSA_REPO_IMAGE" &&
-
-echo "------- Salsa provenance $SALSA_SCAN_REPO uploaded -------" &&
-
-salsa attest \
-  --verify \
-  --repo "$SALSA_SCAN_REPO" \
-  --config salsa-sample.yaml "$SALSA_REPO_IMAGE" &&
-
-echo "------- Attest $SALSA_SCAN_REPO fetch and generated -------"
+  echo "------- Salsa provenance $SALSA_SCAN_REPO generated -------" &&
+  salsa attest \
+    --repoDir "$SALSA_SCAN_REPO_DIR" \
+    --repo "$SALSA_SCAN_REPO" \
+    --config salsa-sample.yaml "$SALSA_REPO_IMAGE" &&
+  echo "------- Salsa provenance $SALSA_SCAN_REPO uploaded -------" &&
+  salsa attest \
+    --verify \
+    --repoDir "$SALSA_SCAN_REPO_DIR" \
+    --repo "$SALSA_SCAN_REPO" \
+    --config salsa-sample.yaml "$SALSA_REPO_IMAGE" &&
+  echo "------- Attest $SALSA_SCAN_REPO fetch and generated -------"
