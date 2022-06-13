a617f1fd96f810672a03c204dd63613a29f1dbe4
ybelMekk <youssef.bel.mekki@nav.no>
2022-06-13 14:33:53 +0200

    feat: better error handling and more informative.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,47 +1,96 @@
 #!/bin/sh -l
 
-REPO_NAME="${INPUT_REPO_NAME##*/}"
-# REPO_DIR="${INPUT_REPO_DIR%/*}"
+setup() {
+  echo "---------- Preparing pico-de-galo SLSA ----------"
 
-if [ -n "$INPUT_DOCKER_USER" ]; then
-  export GITHUB_ACTOR=$INPUT_DOCKER_USER
-fi
-
-if [ -n "$INPUT_IMAGE" ]; then
-  export IMAGE=$INPUT_IMAGE
-fi
-
-GITHUB=$(echo "${INPUT_GITHUB_CONTEXT}" | base64 -w 0)
-RUNNER=$(echo "${INPUT_RUNNER_CONTEXT}" | base64 -w 0)
-ENVS=$(jq -n env | base64 -w 0)
-DOCKER_REGISTRY="${IMAGE%%/*}"
-
-export JAVA_HOME=/opt/java/openjdk
-
-echo "---------- Preparing pico-de-galo slsa for repository: $REPO_NAME ----------"
-echo "$INPUT_DOCKER_PWD" | docker login "$DOCKER_REGISTRY" -u "$GITHUB_ACTOR" --password-stdin
-
-salsa scan \
-  --repo "$REPO_NAME" \
-  --build-context "$GITHUB" \
-  --runner-context "$RUNNER" \
-  --env-context "$ENVS" \
-  --subDir "$INPUT_REPO_SUB_DIR" \
-  --with-deps="$INPUT_DEPENDENCIES" \
-  --remote-run &&
-  salsa attest \
-    --repo "$REPO_NAME" \
-    --subDir "$INPUT_REPO_SUB_DIR" \
-    --remote-run \
-    --key "$INPUT_KEY" \
-    "$IMAGE" &&
-  salsa attest \
-    --verify \
-    --repo "$REPO_NAME" \
-    --subDir "$INPUT_REPO_SUB_DIR" \
-    --remote-run \
-    --key "$INPUT_KEY" \
-    "$IMAGE" && echo "clean up..." && docker logout "$DOCKER_REGISTRY" &&
-  if [ -n "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
-    rm "$GOOGLE_APPLICATION_CREDENTIALS"
+  REPO_NAME="${INPUT_REPO_NAME##*/}"
+  if [ -z "$REPO_NAME" ]; then
+    echo "REPO_NAME is empty"
+    exit 1
   fi
+
+  if [ -n "$INPUT_DOCKER_USER" ]; then
+    export GITHUB_ACTOR=$INPUT_DOCKER_USER
+  fi
+
+  if [ -z "$GITHUB_ACTOR" ]; then
+    echo "GITHUB_ACTOR is not set. Please set it to your GitHub username."
+    exit 1
+  fi
+
+  if [ -n "$INPUT_IMAGE" ]; then
+    export IMAGE=$INPUT_IMAGE
+  fi
+
+  if [ -z "$IMAGE" ]; then
+    echo "IMAGE is not set"
+    exit 1
+  fi
+
+  if [ -z "$INPUT_GITHUB_CONTEXT" ] || [ -z "$INPUT_RUNNER_CONTEXT" ]; then
+    echo "GITHUB_CONTEXT and RUNNER_CONTEXT are required"
+    exit 1
+  fi
+
+  GITHUB=$(echo "${INPUT_GITHUB_CONTEXT}" | base64 -b 0) &&
+    RUNNER=$(echo "${INPUT_RUNNER_CONTEXT}" | base64 -b 0) &&
+    ENVS=$(jq -n env | base64 -b 0)
+
+  DOCKER_REGISTRY="${IMAGE%%/*}"
+  if [ -z "$DOCKER_REGISTRY" ]; then
+    echo "DOCKER_REGISTRY is not set"
+    exit 1
+  fi
+
+  export JAVA_HOME=/opt/java/openjdk
+}
+
+loginDocker() {
+  echo "---------- Logging in to Docker registry: $DOCKER_REGISTRY ----------"
+  echo "$INPUT_DOCKER_PWD" | docker login "$DOCKER_REGISTRY" -u "$GITHUB_ACTOR" --password-stdin
+}
+
+logoutDocker() {
+  echo "---------- Logging out from Docker registry: $DOCKER_REGISTRY ----------"
+  docker logout "$DOCKER_REGISTRY"
+}
+
+cleanUpGoogle() {
+  echo "---------- Clean up Google Cloud files ----------"
+  if
+    [ -n "$GOOGLE_APPLICATION_CREDENTIALS" ] ||
+      [ -n "$CLOUDSDK_AUTH_CREDENTIAL_FILE_OVERRIDE" ] ||
+      [ -n "$GOOGLE_GHA_CREDS_PATH" ]
+  then
+    rm -rvf "$GOOGLE_APPLICATION_CREDENTIALS" "$CLOUDSDK_AUTH_CREDENTIAL_FILE_OVERRIDE" "$GOOGLE_GHA_CREDS_PATH"
+  fi
+}
+
+runSalsa() {
+  echo "---------- Running Salsa for repository: $REPO_NAME ----------" &&
+    salsa scan \
+      --repo "$REPO_NAME" \
+      --build-context "$GITHUB" \
+      --runner-context "$RUNNER" \
+      --env-context "$ENVS" \
+      --subDir "$INPUT_REPO_SUB_DIR" \
+      --with-deps="$INPUT_DEPENDENCIES" \
+      --remote-run &&
+    echo "create and upload attestation" &&
+    salsa attest \
+      --repo "$REPO_NAME" \
+      --subDir "$INPUT_REPO_SUB_DIR" \
+      --remote-run \
+      --key "$INPUT_KEY" \
+      "$IMAGE" &&
+    echo "verify attestation" &&
+    salsa attest \
+      --verify \
+      --repo "$REPO_NAME" \
+      --subDir "$INPUT_REPO_SUB_DIR" \
+      --remote-run \
+      --key "$INPUT_KEY" \
+      "$IMAGE"
+}
+
+setup && loginDocker && runSalsa && logoutDocker && cleanUpGoogle
